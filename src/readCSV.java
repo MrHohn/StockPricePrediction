e6b5abdc4dd6d643e9adee9b2a705c355a95c8ba
MrHohn <zzhonzi@gmail.com>
2015-03-03 10:32:31 -0500

    subtle changes

--- src/readCSV.java
+++ src/readCSV.java
@@ -1,33 +1,33 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 /**
  * Created by Hon on 2/28/2015.
  */
-public class readCSV {
+public class ReadCSV {
     private int N;
-    private double[] price;
-    private String[] info;
+    private double[][] price;
+    private String[] date;
+    private String[][] info;
     private String symbol;
+    private int offset;
 
-    public readCSV(int N, String symbol){
+    public ReadCSV(int N, String symbol, int offset){
         this.N = N;
-        price = new double[N];
+        price = new double[N][1];
+        date = new String[N];
         this.symbol = symbol;
-    }
 
-    public double[] read() {
-        info = new String[1241];
+        info = new String[1241][2];
         try {
             BufferedReader reader = new BufferedReader(new FileReader("stockhistorical.csv"));
             String line = null;
             int count = 0;
             while((line = reader.readLine()) != null){
                 String item[] = line.split(",");
-                info[count] = item[3];
-//                System.out.println(info[count]);
+                info[count][0] = item[3];
+                info[count][1] = item[1];
+//                System.out.printf("%s %s\n", info[count][0], info[count][1]);
                 ++count;
             }
         } catch (Exception e) {
@@ -37,32 +37,44 @@ public class readCSV {
 //        System.out.println(symbol);
         if(symbol.equals("GOOG")){
             for(int i = N - 1; i >= 0; i--){
-                price[i] = Double.parseDouble(info[N - i]);
+                price[i][0] = Double.parseDouble(info[N - i + offset][0]);
+                date[i] = info[N - i + offset][1];
 //                System.out.println(symbol);
             }
         }
         else if(symbol.equals("YHOO")){
             for(int i = N - 1; i >= 0; i--) {
-                price[i] = Double.parseDouble(info[233 + N - i]);
+                price[i][0] = Double.parseDouble(info[233 + N - i + offset][0]);
+                date[i] = info[233 + N - i + offset][1];
             }
         }
         else if(symbol.equals("AAPL")){
             for(int i = N - 1; i >= 0; i--) {
-                price[i] = Double.parseDouble(info[485 + N - i]);
+                price[i][0] = Double.parseDouble(info[485 + N - i + offset][0]);
+                date[i] = info[485 + N - i + offset][1];
             }
         }
         else if(symbol.equals("FB")){
             for(int i = N - 1; i >= 0; i--) {
-                price[i] = Double.parseDouble(info[737 + N - i]);
+                price[i][0] = Double.parseDouble(info[737 + N - i + offset][0]);
+                date[i] = info[737 + N - i + offset][1];
             }
         }
         else if(symbol.equals("MSFT")){
             for(int i = N - 1; i >= 0; i--) {
-                price[i] = Double.parseDouble(info[989 + N - i]);
+                price[i][0] = Double.parseDouble(info[989 + N - i + offset][0]);
+                date[i] = info[989 + N - i + offset][1];
             }
         }
+    }
 
-        info = null;
+    public double[][] readPrice() {
+
         return price;
+    }
+
+    public String[] readDate() {
+
+        return date;
     }
 }
--- src/stockpredict.java
+++ src/stockpredict.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 /**
  * Created by Hon on 2/27/2015.
  */
-public class stockpredict {
+public class StockPredict {
 
-	public static void main(String[] args) {
+    private String symbol;
+    private int n;
+    private int offset;
 
-        Scanner sc = new Scanner(System.in);
+    public String[] date;
 
-        System.out.println("input the ticker of the stock: ");
-        String symbol = sc.nextLine();
-        System.out.println("input the n");
-        int n = sc.nextInt();
+    public StockPredict(String symbol, int n, int offset){
+        this.symbol = symbol;
+        this.n = n;
+        this.offset = offset;
+        date = new String[n];
+    }
+
+    public double[] getPriceVariance() {
 
         int i,j;
         int M = 4;
@@ -21,11 +27,9 @@ public class stockpredict {
         double alpha = 0.005;
         double beta = 11.1;
 
-        System.out.printf("ticker = %s\n", symbol);
-        System.out.printf("M = %d\n", M);
-        System.out.printf("n = %d\n", n);
-        System.out.printf("Alpha = %f\n", alpha);
-        System.out.printf("Beta = %f\n", beta);
+//        System.out.printf("M = %d\n", M);
+//        System.out.printf("Alpha = %f\n", alpha);
+//        System.out.printf("Beta = %f\n", beta);
 
         double x[] = new double[n + 1];
         double t[][];
@@ -34,9 +38,11 @@ public class stockpredict {
         double s[][];
         double lt[][] = new double[M + 1][1];
         double predictprice[][];
+//        String[] date;
 
-        readCSV readClosePrice = new readCSV(n, symbol);
-        t = readClosePrice.read();
+        ReadCSV readClosePrice = new ReadCSV(n, symbol, offset);
+        t = readClosePrice.readPrice();
+        date = readClosePrice.readDate();
 
         /*---------------initialize the training data---------------*/
 
@@ -45,27 +51,28 @@ public class stockpredict {
             x[i] = i + 1;
         }
 
-        System.out.printf("\nthe recent prices are:\n");
-        for(i = 0; i <= n - 1; ++i)
-        {
-            System.out.printf("t[%d] = %f\n", i, t[i][0]);
-        }
+//        System.out.printf("\nthe recent prices are:\n");
+//        for(i = n - 1; i >= 0; --i)
+//        {
+////            System.out.printf("t[%d] = %f\n", i, t[i][0]);
+//            System.out.printf("%s: %f\n", date[i], t[i][0]);
+//        }
 
         /*--------------calculate SUM-φ(xn)-------------------*/
 
         for(i = 0; i < n; i++)
         {
-          for(j = 0; j <= M; j++)
-          {
-            a[j][0] += Math.pow(x[i], j);
-          }
+            for(j = 0; j <= M; j++)
+            {
+                a[j][0] += Math.pow(x[i], j);
+            }
         }
 
-        System.out.printf("\nthe SUM-Phi(xn) is:\n");
-        for(i = 0; i <= M; i++)
-        {
-            System.out.printf("%f\n", a[i][0]);
-        }
+//        System.out.printf("\nthe SUM-Phi(xn) is:\n");
+//        for(i = 0; i <= M; i++)
+//        {
+//            System.out.printf("%f\n", a[i][0]);
+//        }
 
         Matrix A = new Matrix(a);
 
@@ -76,12 +83,12 @@ public class stockpredict {
             b[0][i] = Math.pow(x[n], i);
         }
 
-        System.out.printf("\nthe Phi(x)T is:\n");
-        for(i = 0; i <= M; i++)
-        {
-            System.out.printf("%f  ", b[0][i]);
-        }
-        System.out.printf("\n");
+//        System.out.printf("\nthe Phi(x)T is:\n");
+//        for(i = 0; i <= M; i++)
+//        {
+//            System.out.printf("%f  ", b[0][i]);
+//        }
+//        System.out.printf("\n");
 
         Matrix B = new Matrix(b);
 
@@ -106,16 +113,16 @@ public class stockpredict {
         S = S.inverse();
         s = S.getArray();
 
-        System.out.printf("\nthe Matrix S is:\n");
-        for(i = 0; i <= M; i++)
-        {
-            for(j = 0; j <= M; j++)
-            {
-                System.out.printf("s[%d][%d] = ", i, j);
-                System.out.printf("%4f ", s[i][j]);
-            }
-            System.out.printf("\n");
-        }
+//        System.out.printf("\nthe Matrix S is:\n");
+//        for(i = 0; i <= M; i++)
+//        {
+//            for(j = 0; j <= M; j++)
+//            {
+//                System.out.printf("s[%d][%d] = ", i, j);
+//                System.out.printf("%4f ", s[i][j]);
+//            }
+//            System.out.printf("\n");
+//        }
 
         /*-------------last two parts multiply------------*/
 
@@ -137,14 +144,63 @@ public class stockpredict {
 
         Matrix PP = FT.times(LT).times(beta);
         predictprice = PP.getArray();
-        System.out.printf("\nthe predict price is: \n%f\n", predictprice[0][0]);
+//        System.out.printf("\nthe predict price is: \n%f\n", predictprice[0][0]);
 
         double variance = 1/beta + B.times(S).times(B.transpose()).get(0, 0);
         variance = Math.sqrt(variance);
-        System.out.printf("\nthe predict variance is: \n%f\n", variance);
+//        System.out.printf("\nthe predict variance is: \n%f\n", variance);
 
-        System.out.printf("\nthe predict price is likely in this range: \n%f ~ %f\n", predictprice[0][0] - 3 * variance, predictprice[0][0] + 3 * variance);
+//        System.out.printf("\nthe predict price is likely in this range: \n%f ~ %f\n", predictprice[0][0] - 3 * variance, predictprice[0][0] + 3 * variance);
 
+        double[] priceVariance = {predictprice[0][0], variance};
 
+        return priceVariance;
+    }
+
+	public static int main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("(PS: Enter quit to exit.)");
+
+        String symbol = new String();
+        while(!(symbol.equals("GOOG") || symbol.equals("YHOO") || symbol.equals("AAPL") || symbol.equals("FB") || symbol.equals("MSFT"))){
+            System.out.println("Please enter the ticker of the stock: (GOOG, YHOO, AAPL, FB or MSFT)");
+            symbol = sc.nextLine();
+            if(symbol.equals("quit"))
+                return 0;
+        }
+
+        int n = 0;
+        while((n < 10) || n > 60){
+            System.out.println("Please enter the N: (10 to 60)");
+            n = sc.nextInt();
+//            if(symbol.equals("quit"))
+//                return 0;
+        }
+
+        int offset = -1;
+        while((offset < 0) || offset > 100) {
+            System.out.println("Please enter the offset of the date: (0 to 100)");
+            offset = sc.nextInt();
+//            if(symbol.equals("quit"))
+//                return 0;
+        }
+
+        System.out.printf("\n----------The input parameters are:------------\n");
+        System.out.printf("ticker = %s\n", symbol);
+        System.out.printf("N      = %d\n", n);
+        System.out.printf("offset = %d days\n", offset);
+        System.out.printf("------------------------------------------------\n");
+
+        double[] priceVariance;
+        StockPredict Stock = new StockPredict(symbol, n, offset);
+        priceVariance = Stock.getPriceVariance();
+
+        System.out.printf("\nThe predicted date is the next day of %s\n", Stock.date[n - 1]);
+        System.out.printf("The predicted price is: %f\n", priceVariance[0]);
+        System.out.printf("The predicted variance is: %f\n", priceVariance[1]);
+        System.out.printf("The price would be likely in this range: %f ~ %f\n", priceVariance[0] - 3 * priceVariance[1], priceVariance[0] + 3 * priceVariance[1]);
+
+        return 0;
     }
 }
