aea8a9f297603b0c4697ec1979964bb193d5ce16
MrHohn <zzhonzi@gmail.com>
2015-03-03 19:49:02 -0500

    Added integer judge function.

--- src/TestStockPrediction.java
+++ src/TestStockPrediction.java
@@ -8,20 +8,24 @@ public class TestStockPrediction {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        //        System.out.println("(PS: Enter quit to exit.)");
 
         String symbol = new String();
         while(!(symbol.equals("GOOG") || symbol.equals("YHOO") || symbol.equals("AAPL") || symbol.equals("FB") || symbol.equals("MSFT"))){
             System.out.println("Please enter the ticker of the stock: (GOOG, YHOO, AAPL, FB or MSFT)");
             symbol = sc.nextLine();
-            //            if(symbol.equals("quit"))
-            //                return 0;
         }
 
         int offset = -1;
         while((offset < 0) || offset > 100) {
             System.out.println("Please enter the offset of the date: (0 to 100 days before 2/27/2015)");
-            offset = sc.nextInt();
+            String judge;
+            judge = sc.nextLine();
+            // judge whether user input integer
+            try {
+                offset = Integer.parseInt(judge);
+            } catch (NumberFormatException e) {
+
+            }
         }
 
         System.out.printf("\n----------The input parameters are:------------\n");
@@ -38,7 +42,6 @@ public class TestStockPrediction {
         System.out.printf("The predicted variance is: %f\n", priceVariance[1]);
         System.out.printf("The price would be likely in this range: %f ~ %f\n", priceVariance[0] - 3 * priceVariance[1], priceVariance[0] + 3 * priceVariance[1]);
 
-        //        return 0;
     }
 }
 
