2bacfb7c4e9bd528216206054579e322985c1e24
MrHohn <zzhonzi@gmail.com>
2015-03-06 11:22:02 -0500

    Added comments.

--- src/TestPrediction.java
+++ src/TestPrediction.java
@@ -8,7 +8,7 @@ public class TestPrediction {
     public static void main(String[] args) {
 
         Scanner scMain = new Scanner(System.in);
-        String mode = new String();
+        String mode = new String(); // This variable is used to judge which mode the user select
 
         while(!(mode.equals("auto") || mode.equals("manual") || mode.equals("demo"))){
             System.out.println("Please select runing mode(auto/manual/demo). Enter quit to exit.");
@@ -16,18 +16,18 @@ public class TestPrediction {
 
             if(mode.equals("manual")){
                 ManualStockPrediction predict = new ManualStockPrediction();
-                predict.runPrediction();
+                predict.runPrediction(); // If enter manual, run the manual mode
             }
             else if(mode.equals("auto")){
                 AutoStockPrediction predict = new AutoStockPrediction();
-                predict.runPrediction();
+                predict.runPrediction(); // If enter auto, run the auto mode
             }
             else if(mode.equals("demo")){
                 DemoStockPrediction predict = new DemoStockPrediction();
-                predict.runPrediction();
+                predict.runPrediction(); // If enter demo, run the demo mode
             }
             else if(mode.equals("quit")){
-                break;
+                break; // If the user enter quit, exit
             }
 
             mode = "reset"; // reset the mode variable to continue looping, unless the user want to exit
